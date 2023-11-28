c435ab516a10e1bbea251de6dd07db933e57aa7a
Tim Cheung <tim@cheung.io>
2023-11-28 13:46:36 +0000

    remove obsolete webtranslateit api key
    api key is not working anymore, so do not use it

--- scripts/i18n.ts
+++ scripts/i18n.ts
@@ -1,17 +1,27 @@
 import { access, mkdir, writeFile } from "node:fs/promises";
 import path from "node:path";
 import AdmZip from "adm-zip";
+import dotenv from "dotenv-safe";
+
+try {
+  dotenv.config();
+} catch (error) {
+  if (error instanceof Error) {
+    console.error(error.message);
+  }
+
+  process.exit(1);
+}
 
 const API_URL = "https://webtranslateit.com/api";
-const PUBLIC_API_KEY =
-  process.env.WEBTRANSLATEIT_PUBLIC_API_KEY ||
-  "proj_readonly_rgfBdjfA-x7HAN1OG37b3Q";
+const PROJECT_API_KEY = process.env.WEBTRANSLATEIT_PROJECT_API_KEY;
+
 console.log("start loading language file");
 
 (async () => {
   try {
     const response = await fetch(
-      `${API_URL}/projects/${PUBLIC_API_KEY}/zip_file`,
+      `${API_URL}/projects/${PROJECT_API_KEY}/zip_file`,
     );
 
     if (response.status !== 200)
