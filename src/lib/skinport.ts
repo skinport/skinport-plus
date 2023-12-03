104f5f4260921ce1f5286f6b31ed2ec623c6a969
Tim Cheung <tim@cheung.io>
2023-12-03 22:25:14 +0000

    fix skinport url item slug

--- src/lib/skinport.ts
+++ src/lib/skinport.ts
@@ -1,13 +1,18 @@
 import { steamAppIdNames } from "./steam";
 
+export function getSkinportItemSlug(itemName: string) {
+  return decodeURIComponent(itemName)
+    .toLowerCase()
+    .replace(/[^a-z0-9]+/g, "-")
+    .replace(/^-+|-+$/g, "-")
+    .replace(/^-+|-+$/g, "");
+}
+
 export function getSkinportItemUrl(steamAppId: string, steamItemName: string) {
   const steamAppName =
     steamAppIdNames[steamAppId as keyof typeof steamAppIdNames];
 
   return `https://skinport.com${
     steamAppName !== "cs2" ? `/${steamAppName}` : ""
-  }/item/${decodeURIComponent(steamItemName)
-    .toLowerCase()
-    .replace(/[^\w\s-]/g, "")
-    .replace(/[\s_]+/g, "-")}`;
+  }/item/${getSkinportItemSlug(steamItemName)}`;
 }
